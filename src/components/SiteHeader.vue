c947db9b0e8f9a620fc40ceb87ef4124ce645da1
Minnn <minnnrw311@gmail.com>
2026-09-10 17:20:17 +0800

    處理手機RMD

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,5 +1,12 @@
 <script setup>
+import { ref } from "vue";
 import logo from "../assets/logo.png";
+
+const isMenuOpen = ref(false);
+
+const closeMenu = () => {
+  isMenuOpen.value = false;
+};
 </script>
 
 <template>
@@ -11,20 +18,37 @@ import logo from "../assets/logo.png";
       </RouterLink>
       <p>專營｜國小、國中、高中｜教育、升學、生活陪伴</p>
       <a class="line-button" href="tel:0226310572">預約諮詢</a>
+      <button
+        class="menu-toggle"
+        type="button"
+        :aria-expanded="isMenuOpen"
+        aria-controls="main-navigation"
+        :aria-label="isMenuOpen ? '關閉導覽選單' : '開啟導覽選單'"
+        @click="isMenuOpen = !isMenuOpen"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
     </div>
-    <nav class="nav-shell" aria-label="主要導覽">
-      <RouterLink class="home-link" to="/" aria-label="首頁">
+    <nav
+      id="main-navigation"
+      class="nav-shell"
+      :class="{ 'is-open': isMenuOpen }"
+      aria-label="主要導覽"
+    >
+      <RouterLink class="home-link" to="/" aria-label="首頁" @click="closeMenu">
         <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24">
           <path d="M12 2.8 2.9 10v11.1h6.3v-6.6h5.6v6.6h6.3V10L12 2.8Z" />
         </svg>
       </RouterLink>
-      <RouterLink to="/about">關於大昇</RouterLink>
-      <RouterLink to="/branches">分校資訊</RouterLink>
-      <RouterLink to="/courses">專業課程</RouterLink>
-      <RouterLink to="/news">最新消息</RouterLink>
-      <RouterLink to="/students">大昇專欄</RouterLink>
-      <RouterLink to="/achievements">成果見證</RouterLink>
-      <RouterLink to="/contact">我要先問</RouterLink>
+      <RouterLink to="/about" @click="closeMenu">關於大昇</RouterLink>
+      <RouterLink to="/branches" @click="closeMenu">分校資訊</RouterLink>
+      <RouterLink to="/courses" @click="closeMenu">專業課程</RouterLink>
+      <RouterLink to="/news" @click="closeMenu">最新消息</RouterLink>
+      <RouterLink to="/students" @click="closeMenu">大昇專欄</RouterLink>
+      <RouterLink to="/achievements" @click="closeMenu">成果見證</RouterLink>
+      <RouterLink to="/contact" @click="closeMenu">我要先問</RouterLink>
     </nav>
   </header>
 </template>
